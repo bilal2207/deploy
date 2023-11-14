8e896671867f533b848a5f934fea222d837be444
Your Name <you@example.com>
2023-11-14 11:06:30 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+const data = {
+	color: "red",
+	value: "#f00"
+}
+
+
 app.get('/', (req, res)=>{
     res.send('Winter is coming')
 })
@@ -12,6 +18,10 @@ app.get('/clubs', (req, res)=>{
 
     res.send('<h1>Welcome to the FightClub</h1>')
 
+})
+
+app.get('/data', (req, res)=>{
+    res.json(data);
 })
 
 
